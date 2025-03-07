173b48717108eb85cbcf747ecd73482f1bc15935
OtamaOta <160547995+OtamaOta@users.noreply.github.com>
2025-03-07 19:59:59 -0300

    Delete tarefa04 directory

--- tarefa04/src/CreditCardPayment.java
+++
@@ -1,9 +0,0 @@
-// Estratégia para pagamento via Cartão de Crédito
-public class CreditCardPayment implements PaymentStrategy {
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("Pagamento via Cartão de Crédito no valor de R$" + amount + " processado.");
-        System.out.println("Número do cartão: **** **** **** 1234");
-        System.out.println("Pagamento confirmado!");
-    }
-}
--- tarefa04/src/PaymentStrategy.java
+++
@@ -1,4 +0,0 @@
-// Interface que define o contrato para todas as estratégias de pagamento
-public interface PaymentStrategy {
-    void processPayment(double amount);
-}
